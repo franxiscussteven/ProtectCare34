7031fc0f9a998f93388d931118d37b590ffc321b
leinad05 <ongkowijoyod@gmail.com>
2021-11-29 20:47:32 +0700

    Update Layout Login, Fragment

--- app/src/main/java/com/ubaya/protectcare34/MainActivity.kt
+++ app/src/main/java/com/ubaya/protectcare34/MainActivity.kt
@@ -2,10 +2,19 @@ package com.ubaya.protectcare34
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val fragments: ArrayList<Fragment> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fragments.add(CheckinFragment())
+        fragments.add(HistoryFragment())
+        fragments.add(ProfileFragment())
+
+        viewPager.adapter =MyAdapter(this, fragments)
     }
 }
