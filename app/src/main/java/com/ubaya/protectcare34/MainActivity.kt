afa8e364350a3a9f5bb1c9fdd92b5a9f82812f12
leinad05 <ongkowijoyod@gmail.com>
2021-11-30 15:39:23 +0700

    Update Layout

--- app/src/main/java/com/ubaya/protectcare34/MainActivity.kt
+++ app/src/main/java/com/ubaya/protectcare34/MainActivity.kt
@@ -2,7 +2,9 @@ package com.ubaya.protectcare34
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +18,21 @@ class MainActivity : AppCompatActivity() {
         fragments.add(ProfileFragment())
 
         viewPager.adapter =MyAdapter(this, fragments)
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                bottomNav.selectedItemId = bottomNav.menu[position].itemId
+            }
+        })
+
+        bottomNav.setOnItemSelectedListener {
+            viewPager.currentItem = when(it.itemId) {
+                R.id.itemCheckin -> 0
+                R.id.itemHistory -> 1
+                R.id.itemProfile -> 2
+                else -> 0
+            }
+            true
+        }
     }
 }
