497d0d773dd2e350864b1f9064a201d883d374ca
franxiscussteven <84694556+franxiscussteven@users.noreply.github.com>
2021-12-02 01:12:26 +0700

    Profil sehad
    ws e juga tk update, kurang extract($_post)

--- app/src/main/java/com/ubaya/protectcare34/GlobalData.kt
+++ app/src/main/java/com/ubaya/protectcare34/GlobalData.kt
@@ -2,4 +2,5 @@ package com.ubaya.protectcare34
 
 object GlobalData {
     val places = arrayListOf(Place(0, "Select a place..", "000"))
+    var user = User("",0)
 }
--- app/src/main/java/com/ubaya/protectcare34/ProfileFragment.kt
+++ app/src/main/java/com/ubaya/protectcare34/ProfileFragment.kt
@@ -2,15 +2,18 @@ package com.ubaya.protectcare34
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.fragment_profile.*
 import org.json.JSONObject
 
@@ -54,40 +57,46 @@ class ProfileFragment : Fragment() {
             activity?.finish()
         }
         var username = activity?.intent?.getStringExtra(LoginActivity.EXTRA_USERNAME).toString()
-//        user(username)
-        textName.text = "Name: $username"
+        user(username)
     }
 
-//    fun user(username: String) {
-//        val queue = Volley.newRequestQueue(activity)
-//        val url = "https://ubaya.fun/native/160719019/get_user.php"
-//        val stringRequest = StringRequest(
-//            Request.Method.POST,
-//            url,
-//            Response.Listener {
-//                Log.d("apiresult", it)
-//                val obj = JSONObject(it)
-//                if (obj.getString("result") == "OK") {
-//                    val data = obj.getJSONArray("data")
-//                    for (i in 0 until data.length()) {
-//                        var placeObj = data.getJSONObject(i)
-//                        with(placeObj) {
-//                            var user = User(
-//                                getString("name"),
-//                                getInt("vaccine")
-//                            )
-//                            users.add(user)
-//                        }
-//                    }
-//                    Log.d("usercheck", users.toString())
-//                }
-//            },
-//            Response.ErrorListener {
-//                Log.d("apierror", it.message.toString())
-//            }
-//        )
-//        queue.add(stringRequest)
-//    }
+    fun user(username: String) {
+        val queue = Volley.newRequestQueue(activity)
+        val url = "https://ubaya.fun/native/160719019/get_user.php"
+        val stringRequest = object : StringRequest(
+            Method.POST,
+            url,
+            Response.Listener {
+                Log.d("checkparams", it)
+                val obj = JSONObject(it)
+                if (obj.getString("result") == "OK") {
+                    val data = obj.getJSONArray("data")
+                    for (i in 0 until data.length()) {
+                        var placeObj = data.getJSONObject(i)
+                        with(placeObj) {
+                            GlobalData.user.name = getString("name")
+                            GlobalData.user.vaccine = getInt("vaccine")
+                        }
+
+                        Log.d("usercheck", GlobalData.user.name.toString())
+                        Log.d("usercheck", GlobalData.user.vaccine.toString())
+                    }
+                    textName.text = "Name: " + GlobalData.user.name
+                    textVaccine.text = "Number of vaccination doses: " + GlobalData.user.vaccine
+                }
+            },
+            Response.ErrorListener {
+                Log.d("paramserror", it.message.toString())
+            }
+        ) {
+            override fun getParams(): MutableMap<String, String> {
+                val params = HashMap<String, String>()
+                params["username"] = username
+                return params
+            }
+        }
+        queue.add(stringRequest)
+    }
 
     companion object {
         /**
